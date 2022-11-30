213b9e4f561c186d3dec2d390deaf1620fb2a2a8
Marcin Kala <kalamarcin1@gmail.com>
2022-11-30 11:17:35 +0100

    hange theme/ nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,26 @@
 import React, { useContext } from 'react'
-import { GlobalStoreContext } from '../../Store/GlobalStore' 
+import { GlobalStoreContext } from '../../Store/GlobalStore'
+import light from '../../assets/icon-moon.svg'
+import dark from '../../assets/icon-sun.svg'
+import './Nav.scss'
 
 const Nav = () => {
-    const GlobalStore = useContext(GlobalStoreContext)
+	const GlobalStore = useContext(GlobalStoreContext)
+	const handleSwitchTheme = () => {
+		GlobalStore.switchTheme()
+	}
 
-  return (
-    <>
-    <div className='navbar'>
-    Nav
-    </div>
-    </>
-  )
+	return (
+		<>
+			<div className="navbar">
+				<h1 className="header">devfinder</h1>
+				<div className="toggle" onClick={handleSwitchTheme}>
+				<p>	{GlobalStore.theme === 'dark' ? 'light' : 'dark' }</p>
+					<img className="icon" src={GlobalStore.theme === 'light' ? light : dark } alt="" />
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Nav
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Search.scss'
+import searchIcon from '../../assets/icon-search.svg'
 
 const Search = () => {
   return (
-    <div>Search</div>
+   <>
+   <div className="search-bar">
+    <img className='search-icon' src={searchIcon} alt="" />
+    <input className='search-input' type="text" />
+    <button className='search-btn' >Search</button>
+   </div>
+   </>
   )
 }
 
