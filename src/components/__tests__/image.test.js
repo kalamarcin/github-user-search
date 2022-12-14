737131ef0d313e7da693e121a51035dde1a969c3
Marcin Kala <kalamarcin1@gmail.com>
2022-12-14 10:18:27 +0100

    unit testing

---
+++ src/components/__tests__/image.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import User from '../User/User'
+
+describe('User Component', () => {
+  it('Schuld render User component', () => {
+    render(<User />)
+    
+  })
+})
